a1fc3878050ece273dbcbd27cf423a3ea83b970a
Daniel Silva <danielzcomputacao@gmail.com>
2023-07-28 18:13:23 -0300

    temperature-conversor tests done

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.ts?$": "ts-jest",
+    },
+    transformIgnorePatterns: ["<rootDir>/node_modules/"],
+    coverageReporters: ["json", "text", "lcov", "clover"],
+    testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+};
--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,22 @@
 
 // How to call-----  const name = require('Path');
 
-module.exports = function temperatureConversor(
+export function temperatureConversor(
     initialMeasureUnit: string,
     finalMeasureUnit: string,
     number: number
 ) {
     if (initialMeasureUnit + finalMeasureUnit === "CF") {
-        return ((number * 9) / 5 + 32).toFixed(3);
+        return (number * 9) / 5 + 32;
     } else if (initialMeasureUnit + finalMeasureUnit === "FC") {
-        return (((number - 32) * 5) / 9).toFixed(3);
+        return ((number - 32) * 5) / 9;
     } else if (initialMeasureUnit + finalMeasureUnit === "CK") {
-        return (number + 273.15).toFixed(3);
+        return number + 273.15;
     } else if (initialMeasureUnit + finalMeasureUnit === "KC") {
-        return (number - 273.15).toFixed(3);
+        return number - 273.15;
     } else if (initialMeasureUnit + finalMeasureUnit === "FK") {
-        return (((number - 32) * 5) / 9 + 273.15).toFixed(3);
+        return +(((number - 32) * 5) / 9 + 273.15).toFixed(3);
     } else if (initialMeasureUnit + finalMeasureUnit === "KF") {
-        return (((number - 273.15) * 9) / 5 + 32).toFixed(3);
+        return +(((number - 273.15) * 9) / 5 + 32).toFixed(3);
     }
-};
+}
