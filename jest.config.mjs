3078184649ed4bcde8d8d75ee59a53de916a5955
Daniel Silva <danielzcomputacao@gmail.com>
2023-07-29 18:18:01 -0300

    .prettierrc created

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,10 @@
 export default {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   transform: {
-    "^.+\\.ts?$": "ts-jest",
+    '^.+\\.ts?$': 'ts-jest'
   },
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
-  coverageReporters: ["json", "text", "lcov", "clover"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
-};
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/']
+}
--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,19 @@
 export function temperatureConversor(
   initialMeasureUnit: string,
   finalMeasureUnit: string,
-  number: number,
+  number: number
 ) {
-  if (initialMeasureUnit + finalMeasureUnit === "CF") {
-    return (number * 9) / 5 + 32;
-  } else if (initialMeasureUnit + finalMeasureUnit === "FC") {
-    return ((number - 32) * 5) / 9;
-  } else if (initialMeasureUnit + finalMeasureUnit === "CK") {
-    return number + 273.15;
-  } else if (initialMeasureUnit + finalMeasureUnit === "KC") {
-    return number - 273.15;
-  } else if (initialMeasureUnit + finalMeasureUnit === "FK") {
-    return +(((number - 32) * 5) / 9 + 273.15).toFixed(3);
-  } else if (initialMeasureUnit + finalMeasureUnit === "KF") {
-    return +(((number - 273.15) * 9) / 5 + 32).toFixed(3);
+  if (initialMeasureUnit + finalMeasureUnit === 'CF') {
+    return (number * 9) / 5 + 32
+  } else if (initialMeasureUnit + finalMeasureUnit === 'FC') {
+    return ((number - 32) * 5) / 9
+  } else if (initialMeasureUnit + finalMeasureUnit === 'CK') {
+    return number + 273.15
+  } else if (initialMeasureUnit + finalMeasureUnit === 'KC') {
+    return number - 273.15
+  } else if (initialMeasureUnit + finalMeasureUnit === 'FK') {
+    return +(((number - 32) * 5) / 9 + 273.15).toFixed(3)
+  } else if (initialMeasureUnit + finalMeasureUnit === 'KF') {
+    return +(((number - 273.15) * 9) / 5 + 32).toFixed(3)
   }
 }
